0c86a4561b2daa1ebbfce342ea326efa82b9f33d
adithyanms-debug <adithyanms1007@gmail.com>
2026-01-13 22:46:16 +0530

    Added get mapping

--- src/main/java/com/example/moneymanager/controller/CategoryController.java
+++ src/main/java/com/example/moneymanager/controller/CategoryController.java
@@ -5,10 +5,7 @@ import com.example.moneymanager.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +22,7 @@ public class CategoryController {
         return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
     }
 
+    @GetMapping
     public ResponseEntity<List<CategoryDto>> getCategory() {
         List<CategoryDto> categories = categoryService.getCategoriesForCurrentUser();
         return ResponseEntity.ok(categories);
