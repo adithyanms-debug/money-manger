f1de8a3bab46f6e2887470d02db26fd660df571a
adithyanms-debug <adithyanms1007@gmail.com>
2026-01-15 16:28:12 +0530

    Added methods to expense and income repository

--- src/main/java/com/example/moneymanager/repository/ExpenseRepository.java
+++ src/main/java/com/example/moneymanager/repository/ExpenseRepository.java
@@ -1,7 +1,33 @@
 package com.example.moneymanager.repository;
 
 import com.example.moneymanager.entity.ExpenseEntity;
+import org.springframework.boot.data.autoconfigure.web.DataWebProperties;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
 public interface ExpenseRepository extends JpaRepository<ExpenseEntity, Long> {
+
+    List<ExpenseEntity> findByProfileIdOrderedByDateDesc(Long profileId);
+
+    List<ExpenseEntity> findTop5ByProfileIdOrderByDateDesc(Long profileId);
+
+    @Query("SELECT SUM(e.amount) FROM ExpenseEntity e WHERE i.profile.id = :profileId")
+    BigDecimal findTotalExpenseByProfile(@Param("profileId") Long profileId);
+
+    @Query("SELECT * FROM  tbl_expenses where profile_id = ?1 and date between ?2 and ?3 and name like %?4% ")
+    List<ExpenseEntity> findByProfileIdAndDateBetweenAndNameContainingIgnoreCase(
+            Long profileId,
+            LocalDate startDate,
+            LocalDate rndDate,
+            String keyWord,
+            Sort sort
+    );
+
+    List<ExpenseEntity> findByProfileIdAndDateBetween(Long profileId, LocalDate startDate, Long endDate);
 }
--- src/main/java/com/example/moneymanager/repository/IncomeRepository.java
+++ src/main/java/com/example/moneymanager/repository/IncomeRepository.java
@@ -1,7 +1,32 @@
 package com.example.moneymanager.repository;
 
+import com.example.moneymanager.entity.ExpenseEntity;
 import com.example.moneymanager.entity.IncomeEntity;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
 public interface IncomeRepository extends JpaRepository<IncomeEntity, Long> {
+    List<IncomeEntity> findByProfileIdOrderedByDateDesc(Long profileId);
+
+    List<ExpenseEntity> findTop5ByProfileIdOrderByDateDesc(Long profileId);
+
+    @Query("SELECT SUM(e.amount) FROM ExpenseEntity e WHERE i.profile.id = :profileId")
+    BigDecimal findTotalExpenseByProfile(@Param("profileId") Long profileId);
+
+    @Query("SELECT * FROM  tbl_expenses where profile_id = ?1 and date between ?2 and ?3 and name like %?4% ")
+    List<IncomeEntity> findByProfileIdAndDateBetweenAndNameContainingIgnoreCase(
+            Long profileId,
+            LocalDate startDate,
+            LocalDate rndDate,
+            String keyWord,
+            Sort sort
+    );
+
+    List<IncomeEntity> findByProfileIdAndDateBetween(Long profileId, LocalDate startDate, Long endDate);
 }
