f086f31b62fc7f002372fde63fd8cfd73c239a12
adithyanms-debug <adithyanms1007@gmail.com>
2026-01-20 23:50:14 +0530

    Added a helper method in both income and expense services

--- src/main/java/com/example/moneymanager/service/IncomeService.java
+++ src/main/java/com/example/moneymanager/service/IncomeService.java
@@ -1,9 +1,43 @@
 package com.example.moneymanager.service;
 
+import com.example.moneymanager.dto.ExpenseDto;
+import com.example.moneymanager.dto.IncomeDto;
+import com.example.moneymanager.entity.CategoryEntity;
+import com.example.moneymanager.entity.ExpenseEntity;
+import com.example.moneymanager.entity.IncomeEntity;
+import com.example.moneymanager.entity.ProfileEntity;
+import com.example.moneymanager.repository.IncomeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class IncomeService {
+    private final CategoryService categoryService;
+    private final IncomeRepository incomeRepository;
+
+    private ExpenseEntity toEntity(IncomeDto dto, ProfileEntity profile, CategoryEntity category) {
+        return ExpenseEntity.builder()
+                .id(dto.getId())
+                .name(dto.getName())
+                .icon(dto.getIcon())
+                .date(dto.getDate())
+                .amount(dto.getAmount())
+                .category(category)
+                .profile(profile)
+                .build();
+    }
+
+    private ExpenseDto toDto(IncomeEntity entity) {
+        return ExpenseDto.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .icon(entity.getIcon())
+                .categoryId(entity.getCategory() != null ? entity.getCategory().getId() : null)
+                .amount(entity.getAmount())
+                .date(entity.getDate())
+                .createdAt(entity.getCreatedAt())
+                .updatedAt(entity.getUpdatedAt())
+                .build();
+    }
 }
