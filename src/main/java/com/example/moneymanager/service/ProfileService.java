a62f2371e42e9629d6e5e94ef36fd6f5915096ab
adithyanms-debug <adithyanms1007@gmail.com>
2026-01-28 05:21:49 +0530

    Added the notification service and mad a docker file for the application

--- src/main/java/com/example/moneymanager/service/ProfileService.java
+++ src/main/java/com/example/moneymanager/service/ProfileService.java
@@ -6,6 +6,7 @@ import com.example.moneymanager.entity.ProfileEntity;
 import com.example.moneymanager.repository.ProfileRepository;
 import com.example.moneymanager.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -27,13 +28,16 @@ public class ProfileService {
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
 
+    @Value("{app.activation.url}")
+    private String activationURL;
+
     public ProfileDto registerProfile(ProfileDto profileDto) {
         ProfileEntity newProfile = toEntity(profileDto);
         newProfile.setActivationToken(UUID.randomUUID().toString());
         newProfile = profileRepository.save(newProfile);
 
         //Send activation email
-        String activationLink = "http://localhost:8080/api/v1.0/activate?token=" + newProfile.getActivationToken();
+        String activationLink = activationURL + "/api/v1.0/activate?token=" + newProfile.getActivationToken();
         String subject = "Activate Money manager account";
         String body = "Click on the following account to activate your account: " + activationLink;
         emailService.sendEmail(newProfile.getEmail(), subject, body);
